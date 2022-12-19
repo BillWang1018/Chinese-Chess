da2ead4c7dbe91f79afdf6de1b3c5475fa48b114
Bill <billwang1018@gmail.com>
2022-12-19 15:28:12 +0800

    modified board line, seperate into two files

---
+++ board_handler.py
@@ -0,0 +1,17 @@
+
+
+
+def printBoard():
+    for row in range(10):
+        for col in range(9):
+            if(board_data[row][col] != '0'):
+                print(chess[board_data[row][col]], end='')
+            else:
+                print(board_line[row][col], end='')
+    return
+def getBoardPrintedPosition():
+    return
+def getBoardPosition():
+    return
+def getBoardChess():
+    return
