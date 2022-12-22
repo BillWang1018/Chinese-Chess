6ae2f144d7973ee4ebd538e24178b6846f2ccc4d
Bill <billwang1018@gmail.com>
2022-12-23 01:50:12 +0800

    debug completed

--- checkwalk.py
+++ checkwalk.py
@@ -1,6 +1,14 @@
 def checkB(check, walk, board):
 
-    if check[1] == walk[1] and walk[0] == check[0]+1:
+    if check[1] == walk[1]:
+        if board[check[0]][check[1]] == 'b':
+            if walk[0] == check[0]+1:
+                return True
+            return False
+        elif board[check[0]][check[1]] == 'B':
+            if walk[0] == check[0]-1:
+                return True
+            return False
         return True
     if abs(walk[1]-check[1]) == 1 and check[0] == walk[0]:
         if board[check[0]][check[1]] == 'b' and check[0] <= 4:
@@ -14,23 +22,32 @@ def checkP(check, walk, board):
     if check[0] == walk[0]:
         k1 = min(check[1], walk[1])+1
         k2 = max(check[1], walk[1])
+        n = 0
         for k in range(k1, k2):
             if board[check[0]][k] != '0':
-                if board[walk[0]][walk[1]] != '0':
-                    return True
-                return False
-        return True
+                n += 1
+        if k1 == k2:
+            n = 0
+        if (board[walk[0]][walk[1]] != '0') and n == 1:
+            return True
+        elif (board[walk[0]][walk[1]] == '0') and n == 0:
+            return True
+        return False
     if check[1] == walk[1]:
         k1 = min(check[0], walk[0])+1
         k2 = max(check[0], walk[0])
+        n = 0
         for k in range(k1, k2):
             if board[k][check[1]] != '0':
-                if board[walk[0]][walk[1]] != '0':
-                    return True
-                return False
-        return True
-    else:
+                n += 1
+        if k1 == k2:
+            n = 0
+        if (board[walk[0]][walk[1]] != '0') and n == 1:
+            return True
+        elif (board[walk[0]][walk[1]] == '0') and n == 0:
+            return True
         return False
+    return False
 
 def checkC(check, walk, board):
     if check[0] == walk[0]:
@@ -51,11 +68,11 @@ def checkC(check, walk, board):
 
 def checkM(check, walk, board):
     if abs(walk[0]-check[0]) == 2 and abs(walk[1]-check[1]) == 1:
-        if board[(check[0]+walk[0])/2][check[1]] != '0':
+        if board[int((check[0]+walk[0])/2)][check[1]] != '0':
             return False
         return True
     elif abs(walk[0]-check[0]) == 1 and abs(walk[1]-check[1]) == 2:
-        if board[check[0]][(check[1]+walk[1])/2] != '0':
+        if board[check[0]][int((check[1]+walk[1])/2)] != '0':
             return False
         return True
     return False
@@ -65,26 +82,27 @@ def checkX(check, walk, board):
         return False
     if abs(walk[0]-check[0]) != 2 or abs(walk[1]-check[1]) != 2:
         return False
-    if board[(check[0]+walk[0])/2][(check[1]+walk[1])/2] != '0':
+    if board[int((check[0]+walk[0])/2)][int((check[1]+walk[1])/2)] != '0':
         return False
     return True
 
 def checkS(check, walk, board):
-    if walk[1] < 3 and walk[1] > 5:
-        return False
-    elif walk[0] > 2 and walk[0] < 7:
-        return False
-    if check[0] == walk[0] and abs(walk[1]-check[1]) == 1:
-        return True
-    elif check[1] == walk[1] and abs(walk[0]-check[0]) == 1:
-        return True
-    return False
-
-def checkK(check, walk, board):
-    if walk[1] < 3 and walk[1] > 5:
+    if walk[1] < 3 or walk[1] > 5:
         return False
     elif walk[0] > 2 and walk[0] < 7:
         return False
     elif abs(walk[1]-check[1]) == 1 and abs(walk[0]-check[0]) == 1:
+        return True
+    return False
+
+def checkK(check, walk, board):
+    if walk[1] < 3 or walk[1] > 5:
+        return False
+    elif walk[0] > 2 and walk[0] < 7:
+        return False
+    
+    if check[0] == walk[0] and abs(walk[1]-check[1]) == 1:
+        return True
+    elif check[1] == walk[1] and abs(walk[0]-check[0]) == 1:
         return True
     return False
