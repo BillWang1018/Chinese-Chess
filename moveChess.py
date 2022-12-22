bd86120a3e7d618057e542fc23b6bc10e5e36d5f
Bill <billwang1018@gmail.com>
2022-12-22 13:22:33 +0800

    update

--- moveChess.py
+++ moveChess.py
@@ -5,12 +5,12 @@ import re
 __rowSize__, __colSize__ = 2,4
 
 # from a1 to (0, 0)
-def strToPosition(str:str):
+def strToPosition(str:str) -> tuple:
     x = int(ord(str[0])-97)
     y = int(str[1])-1
     return (x, y)
 
-def moveChess(moveCmd:str, board, turn:bool):
+def moveChess(moveCmd:str, board, turn:bool) -> None:
 
     if(re.search("^[a-j][1-9]\\s[a-j][1-9]$", moveCmd)):
         moveCmd = moveCmd.split(" ")
@@ -21,8 +21,8 @@ def moveChess(moveCmd:str, board, turn:bool):
 
     c = board[startPos[0]][startPos[1]]
     
-    if(checkValid(c, turn)):
+    if(checkValid(c, turn, startPos, endPos)):
         board[startPos[0]][startPos[1]] = '0'
         board[endPos[0]][endPos[1]] = c
-    
-    return True
+    else:
+        raise Exception()
